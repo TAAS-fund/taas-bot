153b58bb3625380f6f887f76bb52276b5d1006bd
Hlib <hlikosh@gmail.com>
2017-06-27 16:47:32 +0300

    garbage cleaned

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function createRegExp(arg) {
     return new RegExp(arg);
 }
 
-//Testing section
+//Dev section
 
 bot.on('message', function(msg){
     const chat = msg.chat.id;
@@ -120,45 +120,24 @@ bot.on('message', function(msg){
 });
 //TODO: Notification logic
 //Notification logic
+
+let json_data;
 fs.readFile('users.json', 'utf8', function(readFileCallback(err, data)){
     if (err){
         console.log(err);
     } else {
-        obj = JSON.parse(data);
-        for(i in obj.users){
-            address = obj.users[i].subcriptions[i].address;
-            request(api+address, function(err, res, body){
-                if(!err && resp.statusCode == 200){
-
-                    let response = [];
-                    let info = JSON.parse(body);
-                    let trans = info.txrefs;
-
-                    for (i in trans){
-                        if (trans[i].tx_input_n == 0){
-                            response.push({date: trans[i].confirmed, value: trans[i].value, incoming: true})
-                        }else{
-                            response.push({date: trans[i].confirmed, value: trans[i].value, incoming: false})
-                        }
-                    }
-                });
-            });
-        }
-    }
+        json_data = JSON.parse(data);
 });
+
+
 //Passed => TRUE
+
 console.log("\"create\" RegExp:",commandList.create.test("/create"));//Testing regexp
 console.log("\"start\" RegExp:",commandList.start.test("/start"));
 console.log("\"delete\" RegExp:",commandList.delete.test("/delete"));
 
-//Definig the data of the message
-bot.on('message', function(msg){
-    //console.log(msg);
-});
-
 //End of testing section
 //TODO: Match inputs of bot with 'commandList' and link them on scenario
-
 
 /*
     Dev part of bot/botfunctions.js
@@ -166,11 +145,6 @@ bot.on('message', function(msg){
     Or not
     Decigion required
 */
-
-//TODO: Saving user data
-function createUser(user) {
-
-}
 
 //Greeting new user
 function greetings(id) {
@@ -181,41 +155,3 @@ function greetings(id) {
 function startNewSubscription(id){
     bot.sendMessage(id, botDialog.startNewSubscription);
 }
-
-//Checking the input of ETH wallet for it's existing in Etherium blockchain
-function checkAddress(id, address, api){
-     request(api+address, function (err,resp) {
-        if(!err && resp.statusCode == 200){
-            bot.sendMessage(id, botDialog.addressApproved);
-        } else{
-            bot.sendMessage(id, botDialog.addressDeclined);
-            startNewSubscription(id)
-        }
-     });
-}
-
-//Checking the name with RegExp (*rulles in development)
-function inputName(id, msg, name){
-    const pattern = createRegExp(commandList.nameValidate);
-
-    if(patten.test(name)){
-        bot.sendMessage(id, botDialog.nameApproved);
-        //bot.sendMessage(id, botDialog.nameApproved);
-    } else {
-        bot.sendMessage(id, botDialog.nameDeclined);
-    }
-}
-
-//Subscription confirmation logic || finalization of user subscription to ETH wallet
-function confirmation(id, name, address, msg){
-    const pattern = new RegExp(rules.confirmationRule);
-    const confirnmation = msg.text;
-
-    if (pattern.test(confirnmation)){
-        //add data saving
-        bot.sendMessage(id, botDialog.nameConfirmed + name + botDialog.addressConfirmed + address);
-    } else {
-        bot.sendMessage(id, botDialog.subscriptionAbandon);
-    }
-}
-
